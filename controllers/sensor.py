f04d9cbfd26654b2bd9c2971df0a9101aba5f312
josephverbeck <joseph.verbeck@nih.gov>
2017-04-19 16:03:33 -0400

    Adding sensor read to controller for time being

--- controllers/sensor.py
+++ controllers/sensor.py
@@ -1,3 +1,5 @@
+import sys
+import Adafruit_DHT
 from flask_restful import Resource
 from flask.json import jsonify
 from sensors.DHT_22 import DHT_22
@@ -5,4 +7,5 @@ from sensors.DHT_22 import DHT_22
 class Sensor(Resource):
 
     def get(self):
-        return jsonify(DHT_22.get_pure_read())
+        humitity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 22)
+        return jsonify({'humitity': humitity, 'temperature': temp})
